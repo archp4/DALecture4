0262e38de541475587e1a8b00482e0fc711f0a86
archp4 <archpatel4@gmail.com>
2024-05-29 11:51:37 -0400

    Data transfroming part 1

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ admissions_df = pd.read_csv("./data/ADMISSIONS.csv")
 #           title="Distribution of Patients per Insurance")
 plot_hist(data=admissions_df, column="admission_type", xLabel="Admission Type", yLabel="Number of Patient",
           title="Admission Type of Patients")
+
+
+chartEvent_df = pd.read_csv("./data/CHARTEVENTS.csv", low_memory=False)
+
+# Transforming Data // pivoting table // rotating table by switch column value to column title
+new_formated_chartEvents = pd.pivot_table(
+    data=chartEvent_df,
+    index=['hadm_id', 'charttime'],
+    columns=['itemid'],
+    values='valuenum'
+)
+
+# Saving DF as CSV
+new_formated_chartEvents.to_csv("Formated_CHARTEVENTS.csv", index=True)
